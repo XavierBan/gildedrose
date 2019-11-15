e93e16dc4257fb255c7954c6c7df57f103312071
iut <iut@W10-DEV-190615>
2019-11-15 15:13:25 +0100

    LegacyInn + test

--- www.github.com/XavierBan/gildedrose/src/test/java/ressources/gildedrose/InnTest.java
+++ www.github.com/XavierBan/gildedrose/src/test/java/ressources/gildedrose/InnTest.java
@@ -6,20 +6,54 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class InnTest {
   @Test
   public void should_return_liste_for_one_Inn_updateQuality() {
-	 ArrayList<Item>  liste = new ArrayList<Item>();
-	 liste.add(new Item("+5 Dexterity Vest", 9, 19));
-	 liste.add(new Item("Aged Brie", 1, 3));
-	 liste.add(new Item("Elixir of the Mongoose", 4, 6));
-	 liste.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-	 liste.add(new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21));
-	 liste.add(new Item("Conjured Mana Cake", 2, 4));
-	 Inn test = new Inn();	 
-	 
-	 test.updateQuality();
-	 assertArrayEquals(liste.toArray(),test.getItems().toArray());
+	  LegacyInn juste = new LegacyInn();
+	  Inn test = new Inn();
+	  Item itemJuste= new Item(null,0,0);
+	  Item itemTest= new Item(null,0,0);
+	  
+	  
+	  
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  juste.updateQuality();
+	  
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  test.updateQuality();
+	  
+	  Iterator<Item> iteJuste=juste.getItems().iterator();
+	  Iterator<Item> iteTest=test.getItems().iterator();
+	  
+	 while(iteJuste.hasNext()) {
+		 itemJuste=iteJuste.next();
+		 itemTest=iteTest.next();
+		 assertEquals(itemJuste.getName(),itemTest.getName());
+		 assertEquals(itemJuste.getQuality(),itemTest.getQuality());
+		 assertEquals(itemJuste.getSellIn(),itemTest.getSellIn());
+		 
+	 }
   }
 }
